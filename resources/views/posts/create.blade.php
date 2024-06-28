59f17d01c082ada3cebf6aedb5fb8fe99e824775
MakotoYoshizaki <yoshizaki.sd22@outlook.jp>
2024-06-28 15:09:27 +0900

    create.bladeとedit.bladeのとりあえずのデザイン第一弾、時間あるとき作り直したい

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,37 +2,39 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <title>コツをシェアしよう！！</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-       
+       <!-- 外部CSSファイルのリンク -->
+        <link rel="stylesheet" href="{{ asset('css/create.blade.css') }}">
     </head>
     <body class="antialiased">
-       <h1>Blog Name</h1>
+    　<div class="container"> <!-- 追加 --><!-- 追加 -->
+       <h1>&#x1F4A1;コツをシェアしよう！&#x1F4A1;</h1>
        <form action="/posts" method="POST">
             @csrf
             <div class="title">
-                <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value={{ old('post.title')}}>
+                <h2>コツ&#x1F4A1;</h2>
+                <input type="text" name="post[title]" placeholder="心拍数をあげるには！" value="{{ old('post.title')}}">
                 <p class='title__error' style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
-                <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も一日お疲れ様でした">{{old('post.body')}}</textarea>
+                <h2>詳しく！</h2>
+                <textarea name="post[body]" placeholder="呼吸をあえて浅くすると良い！">{{old('post.body')}}</textarea>
                 <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <div class="category">
-                <h2>Category</h2>
+                <h3>カテゴリー</h3>
                 <select name="post[category_id]">
                     @foreach($categories as $category)
                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                     @endforeach
                 </select>
             </div>
-            <input type="submit" value="store">
+            <input type="submit" value="シェア！">
         </form>
             <a href="/">戻る</a>
         </div>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,30 +2,32 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <title>もうちょい練る！</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-       
+        
+        <!-- 外部CSSファイルのリンク -->
+        <link rel="stylesheet" href="{{ asset('css/create.blade.css') }}">
     </head>
     <body class="antialiased">
-       <h1>Blog Name</h1>
+        <div class="container"> <!-- 追加 --><!-- 追加 -->
+       <h1>&#x1F35C; もうちょい練る！&#x1F35C;</h1>
        <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
             @method('PUT')
             <div class="title">
-                <h2>Title</h2>
+                <h2>コツ&#x1F4A1;</h2>
                 <input type="text" name="post[title]" placeholder="タイトル" value={{ $post->title }}>
                 <p class='title__error' style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
-                <h2>Body</h2>
+                <h2>詳しく！</h2>
                 <textarea name="post[body]" placeholder="今日も一日お疲れ様でした">{{$post->body}}</textarea>
                 <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type="submit" value="update">
+            <input type="submit" value="アプデ！">
         </form>
             <a href="/posts/{{ $post->id }}">戻る</a>
         </div>
